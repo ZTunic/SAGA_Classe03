4b2e6e621665d5fcf2d9f12aba205d2140255e1c
ZTunic <alessandro.barretta751@gmail.com>
2023-01-10 10:58:34 +0100

    Upload iniziale del progetto

---
+++ src/main/java/com/saga/unipass/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.saga.unipass;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UniPassApplication.class);
+    }
+
+}
---
+++ src/main/java/com/saga/unipass/UniPassApplication.java
@@ -0,0 +1,13 @@
+package com.saga.unipass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniPassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniPassApplication.class, args);
+    }
+
+}
