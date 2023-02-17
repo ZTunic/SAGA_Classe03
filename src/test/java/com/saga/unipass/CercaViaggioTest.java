71e4529470ec55a4d90b6da802a05ea4a103d04d
ZTunic <alessandro.barretta751@gmail.com>
2023-02-17 19:18:22 +0100

    Aggiunta classe testing per la ricerca di viaggi.

---
+++ src/test/java/com/saga/unipass/CercaViaggioTest.java
@@ -0,0 +1,48 @@
+package com.saga.unipass;
+
+import com.saga.unipass.controller.ViaggioController;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class CercaViaggioTest {
+
+    private static ViaggioController viaggioController;
+
+    @BeforeAll
+    public static void init() {
+        viaggioController = new ViaggioController();
+    }
+
+    @Test
+    public void valoreDataNonValido() {
+        ArrayList<String> parametriNonValidi;
+
+        parametriNonValidi = viaggioController.controlloValoriRicercaViaggio("Scafati",
+                "2022/02/14 15:00","8");
+
+        assertTrue(parametriNonValidi.contains("Data non valida."));
+    }
+
+    @Test
+    public void valorePrezzoNonValido() {
+        ArrayList<String> parametriNonValidi;
+
+        parametriNonValidi = viaggioController.controlloValoriRicercaViaggio("Pagani",
+                "2023/12/21 15:30","60");
+
+        assertTrue(parametriNonValidi.contains("Prezzo non valido."));
+    }
+
+    @Test
+    public void creazioneViaggioCorretta() {
+        ArrayList<String> parametriNonValidi;
+
+        parametriNonValidi = viaggioController.controlloValoriRicercaViaggio("Pompei",
+                "2023/02/18 16:30","10");
+
+        assertTrue(parametriNonValidi.isEmpty());
+    }
+}
